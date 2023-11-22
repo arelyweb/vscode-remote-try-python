24e29f2ca8b4a75f1bb405ab8b4dc09ffdfb859a
Arely González <73667815+arelyweb@users.noreply.github.com>
2023-11-22 19:15:24 +0000

    juego con copilot

--- app.py
+++ app.py
@@ -2,10 +2,35 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
+import random
+def juego_piedra_papel_tijera():
+    opciones = ["piedra", "papel", "tijera"]
+    opciones_dict = {"piedra": "tijera", "papel": "piedra", "tijera": "papel"}
 
-from flask import Flask
-app = Flask(__name__)
+    print("Bienvenido al juego de piedra, papel o tijera")
+    print("Para jugar, ingresa una de las siguientes opciones: piedra, papel o tijera")
+    print("Para salir, ingresa la palabra salir")
+    print("")
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+    while True:
+        jugador = input("Ingresa tu jugada: ")
+        jugador = jugador.lower()
+        computadora = random.choice(opciones)
+
+        if jugador == "salir":
+            break
+        elif jugador not in opciones:
+            print("Ingresa una opción válida")
+            print("")
+        elif jugador == computadora:
+            print("Empate!")
+            print("")
+        elif opciones_dict[jugador] == computadora:
+            print("Ganaste!")
+            print("")
+        else:
+            print("Perdiste!")
+            print("")
+    print("Gracias por jugar!")
+
+juego_piedra_papel_tijera()
